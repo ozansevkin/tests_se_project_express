1c07b70cc9fc304bfe0cbb03d89f3c34b8f4389c
Ozan Sevkin <sevkinozan@gmail.com>
2023-07-01 23:24:15 +1000

    update dependencies and format code

--- src/render.js
+++ src/render.js
@@ -1,1 +1,6 @@
-import i18next from'i18next';const render=a=>{console.log('\x1B[1;31m%s\x1B[0m','Correct the errors:'),a.forEach((a,b)=>console.log(`${b+1}. ${i18next.t(a.id,a.values)}`))};export default render;
+import i18next from "i18next";
+const render = (a) => {
+  console.log("\x1B[1;31m%s\x1B[0m", "Correct the errors:"),
+    a.forEach((a, b) => console.log(`${b + 1}. ${i18next.t(a.id, a.values)}`));
+};
+export default render;
